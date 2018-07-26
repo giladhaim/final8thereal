fdf84529f70386a0e7ec67c7bf510e0b57e003c9
gilad <giladhaim3@gmail.com>
2018-07-26 22:29:34 +0300

    hi

---
+++ final8.py
@@ -0,0 +1,11 @@
+age = input("how old are you? ")
+if age:
+    age =int(age)
+    if age >=21:
+    print("come and drink!")
+    elif age >=18:
+    pritn("enter and dont get intoxicated")
+    else:
+    print("you can't enter you little")
+else:
+print("please! enter your name")    
